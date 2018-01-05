1ed022b4e924757930c84eb8ac4fdcd1844f66c3
rainiera <rainier@utexas.edu>
2018-01-05 02:45:43 -0600

    about modal

--- js/TopBar.js
+++ js/TopBar.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import {Level, LevelLeft, LevelRight, LevelItem, Subtitle} from 'bloomer'
 
-const TopBar = () => (<Level>
-  <LevelLeft>
-    <LevelItem>
-      <Subtitle tag='p' isSize={2}>
-        <strong>oblique strategies</strong>
-      </Subtitle>
-    </LevelItem>
-  </LevelLeft>
-  <LevelRight>
-    <LevelItem>
-      <i className="fa fa-question-circle-o fa-2x" aria-hidden="true"></i>
-      {'\u00A0'}
-      {'\u00A0'}
-      <i className="fa fa-square-o fa-2x" aria-hidden="true"></i>
-      {'\u00A0'}
-      {'\u00A0'}
-      <i className="fa fa-font fa-2x" aria-hidden="true"></i>
-      {'\u00A0'}
-      {'\u00A0'}
-      <i className="fa fa-random fa-2x" aria-hidden="true"></i>
-    </LevelItem>
-  </LevelRight>
-</Level>);
+export default class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'aboutModalActivated': false
+    }
+  }
+  activateAboutModal() {}
 
-export default TopBar;
+  render() {
+    return (<Level>
+      <LevelLeft>
+        <LevelItem>
+          <Subtitle tag='p' isSize={2}>
+            <strong>oblique strategies</strong>
+          </Subtitle>
+        </LevelItem>
+      </LevelLeft>
+      <LevelRight>
+        <LevelItem>
+          <a href="#" onclick="">
+            <i className="fa fa-question-circle-o fa-2x" aria-hidden="true"></i>
+          </a>
+          {'\u00A0'}
+          {'\u00A0'}
+          <i className="fa fa-square-o fa-2x" aria-hidden="true"></i>
+          {'\u00A0'}
+          {'\u00A0'}
+          <i className="fa fa-font fa-2x" aria-hidden="true"></i>
+          {'\u00A0'}
+          {'\u00A0'}
+          <i className="fa fa-random fa-2x" aria-hidden="true"></i>
+        </LevelItem>
+      </LevelRight>
+    </Level>);
+  }
+}
